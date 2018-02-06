b7f96cdccc9cd41d23c5cd1cc21c91f9f29416f0
github username <github email address>
2018-02-06 16:54:20 -0500

    made progress

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,24 +7,42 @@ class Container extends React.Component {
   state = {
     loans: [],
     status: 'fundraising',
-    filter: 0
+    filter: 0,
+    nextPage: 1,
+    loadable: true
   }
 
   componentDidMount() {
     this.loadLoans()
+    document.addEventListener('scroll', this.handleScroll)
   }
 
-  loadLoans() {
-    fetch(`http://api.kivaws.org/v1/loans/search.json?status=${this.state.status}`)
+
+  handleScroll = () => {
+    let scrollBottom = window.innerHeight + window.pageYOffset
+    let loadHeight = document.body.scrollHeight - (2 * window.innerHeight)
+    if (scrollBottom > loadHeight && this.state.loadable ) {
+      this.setState({
+        loadable: false
+      }, () => setTimeout(() => this.setState({ loadable: true }), 1000))
+      this.loadLoans()
+    }
+  }
+
+  loadLoans = () => {
+    fetch(`http://api.kivaws.org/v1/loans/search.json?status=${this.state.status}&page=${this.state.nextPage}`)
     .then(res => res.json())
-    .then(json => this.setState({
-      loans: json.loans
-    }))
+    .then(json => this.setState((prevState) => {
+      return {
+      loans: [...prevState.loans, ...json.loans],
+      nextPage: ++prevState.nextPage
+    }}))
   }
 
   handleSearch = (searchVal) => {
     this.setState({
-      status: searchVal
+      status: searchVal,
+      nextPage: 1
     }, this.loadLoans)
   }
 
@@ -37,7 +55,7 @@ class Container extends React.Component {
   render() {
     return(
       <div>
-        <SearchBar filter={this.state.filter} handleFilter={this.handleFilter} handleSearch={this.handleSearch}/>
+        <SearchBar filter={this.state.filter} handleFilter={this.handleFilter} handleSearch={this.handleSearch} loadLoans={this.loadLoans}/>
         <LoanList loans={this.state.loans} filter={this.state.filter} />
       </div>
     )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,22 @@ import React from 'react'
 import Filter from './Filter'
 import Search from './Search'
 
+
 const SearchBar = (props) => {
+
+  const handleClick = () => {
+    props.loadLoans()
+  }
+
+  const LoadMore = () => {
+    return(<button onClick={handleClick}>Load More</button>)
+  }
 
   return(
     <div>
       <Filter handleFilter={props.handleFilter} />
       <Search handleSearch={props.handleSearch}/>
+      {LoadMore()}
     </div>
   )
 
